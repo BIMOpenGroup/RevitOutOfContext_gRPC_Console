f3ddcc1bc64e920c786733c16849dcacab162040
ababinov <a.n.babinov@we-on.com>
2024-01-31 20:19:40 +0300

    revit test but not work

---
+++ RevitGrpcExecutorConsoleAddin/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("RevitExecuteConsoleAddin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RevitExecuteConsoleAddin")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("64400b04-9dbe-4464-9a6f-4276ac34d338")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RevitGrpcExecutorConsoleAddin/RevitGrpcAddinApp.cs
@@ -0,0 +1,79 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Events;
+using System;
+using System.Collections.Generic;
+using Grpc.Net.Client;
+using Grpc.Net.Client.Web;
+using GrpcGreeterClient;
+using System.Net.Http;
+
+namespace RevitGrpcExecutorConsoleAddin
+{
+    [Transaction(TransactionMode.Manual)]
+    [RegenerationAttribute(RegenerationOption.Manual)]
+    public class RevitGrpcAddinApp : IExternalApplication
+    {
+        public static UIControlledApplication _uiControlApplication;
+        public string _userName;
+        public Result OnStartup(UIControlledApplication uiControlApplication)
+        {
+            try
+            {
+                uiControlApplication.Idling += OnIdling;
+                _uiControlApplication = uiControlApplication;
+                _userName = Environment.UserName;
+                return Result.Succeeded;
+            }
+            catch (Exception ex)
+            {
+                TaskDialog.Show("fail", ex.Message);
+                return Result.Cancelled;
+            }
+        }
+
+        public Result OnShutdown(UIControlledApplication uiControlApplication)
+        {
+            uiControlApplication.Idling -= OnIdling;
+            //_serverDispatcher.ReciveData -= OnReciveData;
+            return Result.Succeeded;
+        }
+
+        private void OnIdling(object sender, IdlingEventArgs e)
+        {
+            try
+            {
+                var channel = GrpcChannel.ForAddress("https://localhost:5064", new GrpcChannelOptions
+                {
+                    HttpHandler = new GrpcWebHandler(new HttpClientHandler())
+                });
+
+                var client = new Greeter.GreeterClient(channel);
+
+                var userName = Environment.UserName;
+
+                var text = "Console.ReadLine()";
+                var reply = client.SayHello(
+                                  new HelloRequest { Name = userName, Text = text });
+
+                //var uiapp = (UIApplication)sender;
+                //var uidoc = uiapp.ActiveUIDocument;
+                //var selection = uidoc?.Selection;
+                //var doc = uidoc?.Document;
+                //if (selection != null)
+                //{
+                //    var elemsIds = selection.GetElementIds();
+                //    if (elemsIds.Count > 0)
+                //    {
+
+                //    }
+                //}
+            }
+            catch (Exception ex)
+            {
+                TaskDialog.Show("Exception", ex.Message);
+            }
+        }
+    }
+}

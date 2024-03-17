7cae42d32a3f409127999d55064dd44049c1403d
ababinov <a.n.babinov@we-on.com>
2024-03-17 11:21:27 +0300

    remove static command

--- RevitOutOfContext_gRPC_Server/GrpcServerConsole/Common.cs
+++ RevitOutOfContext_gRPC_Server/GrpcServerConsole/Common.cs
@@ -3,19 +3,19 @@
     public class Common
     {
         public static ClientCollection clientCollection = new ClientCollection();
-        public static string currentCommand = "";
+        //public static string currentCommand = "";
 
         public static void ChangeCommand(string newCommand)
         {
-            if (currentCommand != newCommand)
-            {
+            //if (currentCommand != newCommand)
+            //{
                 var collection = clientCollection.GetCollection();
                 foreach (var item in collection)
                 {
-                    clientCollection.Update(item.Key, false);
+                    clientCollection.Update(item.Key, true, newCommand);
                 }
-            }
-            currentCommand = newCommand;
+            //}
+            //currentCommand = newCommand;
         }
     }
 }
